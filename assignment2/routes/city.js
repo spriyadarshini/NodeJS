70628b43c566dda00b23e7a478d8cf0719626592
Priyadarshini Sarode <psarode@equinix.com>
2015-03-03 12:32:14 -0800

    changes

--- assignment2/routes/city.js
+++ assignment2/routes/city.js
@@ -4,6 +4,7 @@
  */
 var router = require('express').Router();
 var restify = require('restify');
+var _ = require('lodash');
 
 router.get('/',function(req,res){
     // Creates a JSON client
@@ -11,19 +12,20 @@ router.get('/',function(req,res){
         url: 'http://api.geonames.org/'
     });
     client.get('/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo', function(err, request, response, obj) {
-        console.log("obj "+JSON.stringify(obj));
-
-        if(typeof obj.status.message !='undefined') {
-            res.render('city', {city: 'Details Not found'});
+      
+        if(typeof obj.status !='undefined') {
+            res.render('city', {city: obj.status.message});
         }else{
             var arrayCities = [];
             for (var i in obj.geonames) {
                 for (var j in obj.geonames[i]) {
-                    objCity = {"name": obj.geonames[i][j].toponymName, "countryCode": obj.geonames[i][j].countrycode}
+                    console.log ("obj.geonames[i] "+obj.geonames[i])
+                    objCity = {"name": obj.geonames[i].toponymName, "countryCode": obj.geonames[i].countrycode}
                     arrayCities.push(objCity);
                 }
             };
-            res.render('city', {city: JSON.stringify(arrayCities)});
+            var newCityList = _.pluck(_.uniq(arrayCities, 'name'),'name');
+            res.render('city', {city: JSON.stringify(newCityList)});
         }
     });
 })
